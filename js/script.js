39fcaa96ff28bdab63c733b21262341a78a71377
Oleg Markov <o.mark@mail.ru>
2026-02-25 11:51:14 +0200

    wedding

---
+++ js/script.js
@@ -0,0 +1,93 @@
+const BOT_TOKEN = '8381550218:AAEs2RgcZxOXAKAJva2PHzeC-ahnk-yD2cs';
+const CHAT_ID = '228516010';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const observer = new MutationObserver(() => {
+    const form = document.querySelector('#form899457406');
+    const button = document.querySelector('button');
+
+
+    if (button && !button.dataset.telegramAttached) {
+      button.dataset.telegramAttached = 'true'; 
+
+      button.addEventListener('click', async function (e) {
+        e.preventDefault();
+        
+        const drinks = document.querySelector('.t-checkboxes__hiddeninput').value || '';
+        
+        const check = document.querySelectorAll('.t-radio');
+
+        const name = document.querySelector('#nm-1741963235995').value.trim();
+        console.log(name);
+        
+        let attendance;
+        let secondDay;
+
+        for (let i = 0; i < 4; i++) {
+            if (check[i].checked === true && check[i].value === 'Да') {
+                console.log(`${check[i].ariaLabel} Да`);
+                if (check[i].ariaLabel === 'Сможете ли Вы присутствовать на торжестве?') {
+                    attendance = `Да`;
+                } else if (check[i].ariaLabel === 'Будете ли Вы присутствовать на втором дне свадьбы?') {
+                    secondDay = `Да`}
+            } else if (check[i].checked === true && check[i].value === 'Нет') {
+                console.log(`${check[i].ariaLabel} Нет`);
+                if (check[i].ariaLabel === 'Сможете ли Вы присутствовать на торжестве?') {
+                    attendance = `Нет`;
+                } else if (check[i].ariaLabel === 'Будете ли Вы присутствовать на втором дне свадьбы?') {
+                    secondDay = `Нет`}
+            };
+        }
+
+        const data = {};
+
+        for (let [key, val] of Object.entries(data)) {
+          if (Array.isArray(val)) {
+            val = val.join(', ');
+          }
+          if (val && val.trim()) {
+            message += `• <b>${key}:</b> ${val}\n`;
+          }
+        }
+        
+        const message = `
+📝 Новая анкета от гостя!
+
+👤 Имя: ${name}
+✅ Присутствие: ${attendance}
+🗓 Второй день: ${secondDay}
+🍷 Напитки: ${drinks}
+
+${new Date().toLocaleString('ru-RU')}`.trim();
+
+        const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+
+        try {
+          const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              chat_id: CHAT_ID,
+              text: message,
+              parse_mode: 'HTML',
+            }),
+          });
+
+          if (response.ok) {
+            alert(
+              'Анкета отправлена! Спасибо, что подтвердили участие ❤️ Мы вас ждём!'
+            );
+            form.reset();
+          } else {
+            alert(
+              'Что-то пошло не так... Попробуйте позже или напишите нам напрямую.'
+            );
+          }
+        } catch (err) {
+          return;
+        }
+      });
+    }
+  });
+  observer.observe(document.body, { childList: true, subtree: true });
+});
